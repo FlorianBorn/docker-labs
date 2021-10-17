dbea5e804bb1eee0442c4a925ecac3dd41683600
FlorianB <foo@bar-blargh.com>
2021-10-17 14:42:33 -0700

    update apache_basic_https

--- apache_basic_https/run.sh
+++ apache_basic_https/run.sh
@@ -6,9 +6,11 @@ echo "or https://localhost:8443 for HTTPS"
 
 docker run \
   -d \
+  --rm \
   -p 8080:80 \
   -p 8443:443 \
   -v $(pwd)/httpd.crt:/etc/pki/tls/certs/httpd.crt \
   -v $(pwd)/httpd.key:/etc/pki/tls/private/httpd.key \
   -v $(pwd)/ssl.conf:/etc/httpd/conf.d/ssl.conf \
+  -v $(pwd)/public-html/index.html:/var/www/html/index.html \
   apache_basic_https
