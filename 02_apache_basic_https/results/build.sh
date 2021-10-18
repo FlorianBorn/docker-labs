e3f937a7a2de3dd653a47e8fa5960549b022b10f
FlorianB <foo@bar-blargh.com>
2021-10-18 14:35:56 -0700

    refactor directory structure, add 01_nfs lab

---
+++ 02_apache_basic_https/results/build.sh
@@ -0,0 +1,1 @@
+docker build -t 01_apache_basic_https -f Dockerfile.server .
---
+++ 02_apache_basic_https/results/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "starting apache..."
+echo "you can access apache on localhost:8080 for HTTP"
+echo "or https://localhost:8443 for HTTPS"
+
+docker run \
+  -d \
+  --rm \
+  --name apache_server \
+  -p 8080:80 \
+  -p 8443:443 \
+  -v $(pwd)/httpd.crt:/etc/pki/tls/certs/httpd.crt \
+  -v $(pwd)/httpd.key:/etc/pki/tls/private/httpd.key \
+  -v $(pwd)/ssl.conf:/etc/httpd/conf.d/ssl.conf \
+  -v $(pwd)/public-html/index.html:/var/www/html/index.html \
+  01_apache_basic_https
